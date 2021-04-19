6a607d1796a7267015802086c04d90dd98a5e13c
Emmanuel Pali <39196449+Thesixth@users.noreply.github.com>
2021-04-19 11:53:07 -0700

    depolyment prep

--- backend/data/products.js
+++ backend/data/products.js
@@ -40,6 +40,34 @@ const products = [
         description: "The modern King, this sneaker is made specifically with you in mind. Photo Credit ",
         price: 750,
         countInStock: 5
+    },
+    {
+        name: "Sole Feet",
+        imageUrl: "https://cdn.pixabay.com/photo/2014/10/28/16/55/shoe-506826_960_720.jpg",
+        description: "This is Dark Black's premium corporate style. It is made from algea. Look good while playing your part in saving our planet.",
+        price: 350,
+        countInStock: 10
+    },
+    {
+        name: "King James",
+        imageUrl: "https://cdn.pixabay.com/photo/2013/07/12/18/20/shoes-153310_960_720.png",
+        description: "The modern King, this sneaker is made specifically with you in mind. Photo Credit ",
+        price: 750,
+        countInStock: 5
+    },
+    {
+        name: "Sole Feet",
+        imageUrl: "https://cdn.pixabay.com/photo/2014/10/28/16/55/shoe-506826_960_720.jpg",
+        description: "This is Dark Black's premium corporate style. It is made from algea. Look good while playing your part in saving our planet.",
+        price: 350,
+        countInStock: 10
+    },
+    {
+        name: "King James",
+        imageUrl: "https://cdn.pixabay.com/photo/2013/07/12/18/20/shoes-153310_960_720.png",
+        description: "The modern King, this sneaker is made specifically with you in mind. Photo Credit ",
+        price: 750,
+        countInStock: 5
     }
 ];
 
