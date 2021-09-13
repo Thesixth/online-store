7944a76419898d042ba5d56e2801715811c3578c
Emmanuel Pali <39196449+Thesixth@users.noreply.github.com>
2021-09-12 23:57:05 -0700

    fixing app crash

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,7 @@ if(process.env.NODE_ENV === "production") {
     })
 }
 
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`server on ${PORT}`));
+app.listen(process.env.PORT || 5000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
